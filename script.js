1c6ba9d9321bd42774fa07263ba39d051386cad4
Kalakkal Karthikeyan <kalakkalkarthi6838@gmail.com>
2025-03-07 20:35:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+function getUnknownReply() {
+    let replies = [
+        "Hmm... I'm not sure about that, but I can learn!",
+        "That’s interesting! Tell me more.",
+        "I don’t know that yet, but Karthikeyan can teach me!",
+        "Good question! I’ll find out soon.",
+        "Haha, I need to learn that!"
+    ];
+    return replies[Math.floor(Math.random() * replies.length)];
+}
+
 function sendMessage() {
     let userInput = document.getElementById("userInput").value.toLowerCase();
     let chatbox = document.getElementById("chatbox");
 
-    let response = responses[userInput] || "I don’t know that yet! Ask something else.";
+    // Search for response in responses1.js and responses2.js
+    let response = responses[userInput] || extraResponses[userInput] || getUnknownReply();
 
     chatbox.innerHTML += `<p><b>You:</b> ${userInput}</p>`;
     chatbox.innerHTML += `<p><b>Kutty:</b> ${response}</p>`;
